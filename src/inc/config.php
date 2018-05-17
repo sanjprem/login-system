7a19de9eaefb10929af7670d80f93c50eac95ce4
Sanj Prem <sanj.tools@gmail.com>
2018-05-17 11:39:37 -0400

    added config; added login/reg pages; added header/footer; styled form

---
+++ src/inc/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    // file won't load if not defined
+    if(!defined('__CONFIG__')) {
+        exit('Access denied.');
+    }
+
+?>
---
+++ src/login.php
@@ -0,0 +1,25 @@
+<?php include_once "partials/header.php"; ?>
+
+<div class="container">
+    <form action="" method="post" class="js-login mt-5">
+        <div class="login-icon d-table p-3 rounded-circle">
+            <img src="/img/logo-icon.png" title="UXFocus Icon">
+        </div>
+        <div class="jumbotron m-auto py-4 border-light">
+            <h2 class="text-muted text-uppercase text-center py-3">Login </h2>
+            <div class="form-group mt-3">
+                <label for="email" class="form-text text-muted text-uppercase">Email</label>
+                <input type="text" class="form-control form-control-lg" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password" class="form-text text-muted text-uppercase">Password</label>
+                <input type="password" class="form-control form-control-lg" id="password" name="password" required>
+            </div>
+            <div class="text-center"> 
+                <input type="submit" class="btn text-white btn-lg btn-block mt-4" id="login" name="loginBtn" value="Sign In">
+            </div>
+        </div>
+    </form>
+</div>
+
+<?php include_once "partials/footer.php"; ?>
