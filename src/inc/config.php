883f084615ae70c67fbceaba4ac4c7a91c60b1f7
Sanj Prem <sanj.tools@gmail.com>
2018-05-22 18:55:45 -0400

    added filter class

--- src/inc/config.php
+++ src/inc/config.php
@@ -5,7 +5,12 @@
         exit('Access denied.');
     }
 
+    // allow errors
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+
     include_once "classes/database.php";
+    include_once "classes/filter.php";
 
     $con = Database::getConnection();
 
